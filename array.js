bbda7a65cd8a1d312b8adf9006d5a7b5092d8606
Rohit Nikhade <rohit.nikhade@logiquad.com>
2021-01-31 17:56:33 +0530

    finished array.js

--- array.js
+++ array.js
@@ -1,11 +1,18 @@
 var a=[1,2,3,14,4,54];
-console.log(a);
+//console.log(a);
 var cars = [
     "Saab",
     "Volvo",
     "BMW"
   ];
-  cars[1]="tata"
-  cars.push("car");
-  cars.pop();
-  console.log(cars);
+//   cars[1]="tata"
+//   cars.push("car");
+//   cars.pop();
+delete cars[0];
+  //console.log(cars);
+
+var a1=[1,2,3,4,5];
+var a2=[2,12,0,14,15];
+var a3=[11,12,13,14,"s"];
+var a4=a1.concat(a2,a3);
+console.log(a4);
